c598f17b7256c417226cef3bd9dae74086a62794
Tindi Sommers <78483332+tindi-plus@users.noreply.github.com>
2025-01-19 11:00:47 +0100

    Create feed_provider.dart

---
+++ lib/providers/feed_provider.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'feed_provider.g.dart';
+
+@Riverpod(dependencies: [])
+String postId(Ref ref) {
+  throw UnimplementedError();
+}
